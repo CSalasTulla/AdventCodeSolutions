56d423414808cee6c7c7958f2180b97cfaab87d1
CSalasTulla <carlos.salas.tulla@gmail.com>
2022-12-25 19:01:27 +0100

    Update comments on day6b

--- Day6/day6b.cpp
+++ Day6/day6b.cpp
@@ -21,26 +21,26 @@ int j,k;
 
 int main()
 {
-    getline(cin,str);                                   //Input of the string pasted on the command line
+getline(cin,str);                               //Input of the string pasted on the command line
 
-    for(int i = 0; i < str.length(); i++)               //for loop to go through the whole string
+    for(int i = 0; i < str.length(); i++)       //for loop to go through the whole string
     {
-        bool repeated = false;                          // setting the boolean variable "repeated" to false to make sure every time we go trhought the loop we set it again to false 
-        for(j = i; j < 14+i;j++)                        //for loop to go through the 14 next letters 
+        bool repeated = false;                  // setting the boolean variable "repeated" to false to make sure every time we go throught 
+        for(j = i; j < 14+i;j++)                //the loop we set it again to false for loop to go through the 14 next letters 
         {
-            for(k = i; k < 14+i;k++)                    //Second for loop to go throught the 14 next letters to compared them with the ones from the firts loop and check if they the letters are repeated
-            {
-                if (j != k && str[j] == str[k])         //(j != k) makes sure that we are not counting as repeated the 2 same characters(if j=k then it is going to be repeated obviously and we want to exclude this case from counting it repeated)
-                {
+            for(k = i; k < 14+i;k++)            //Second for loop to go throught the 14 next letters to compared them with the ones from 
+            {                                   //the firts loop and check if they the letters are repeated
+                if (j != k && str[j] == str[k]) //(j != k) makes sure that we are not counting as repeated the 2 same characters (if j=k then
+                {                               //it is going to be repeated obviously and we want to exclude this case from counting it repeated)
                     repeated = true;
                     break;
                 }
             }
-            if (repeated == true) { break; }            //Breakig the next loop to make sure we leave both loops and we go to the next character
+            if (repeated == true) { break; }    //Breakig the next loop to make sure we leave both loops and we go to the next character
         }
 
-        if(repeated == false)                           //This would be the case we go through the whole 14 letters and we do not have repeated letter
-        {                                               //We know this because we have the boolean variable set to false, meaning we have not foud any repeated letter
+        if(repeated == false)                   //This would be the case we go through the whole 14 letters and we do not have repeated letter
+        {                                       //We know this because we have the boolean variable set to false, meaning we have not foud any repeated letter
             cout << i + 14 << endl;
             return 0;
         }
